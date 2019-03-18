5919c69d2086f338f257f0e1abb8cac0aca65c95
Fernando Moreira <nandomoreira.me@gmail.com>
2019-03-18 13:47:09 -0300

    gatsby config and plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,11 @@ const {
   homepage,
   name,
   repository,
+  title,
   version,
 } = require('./package.json')
 const rupture = require('rupture')
+const path = require('path')
 
 const isProduction = process.env.NODE_ENV === 'production'
 const siteUrl = isProduction ? homepage : 'http://localhost:8000'
@@ -19,13 +21,34 @@ module.exports = {
     name,
     repository,
     siteUrl,
+    social: {
+      facebook: 'oseunando',
+      twitter: 'oseunando',
+      instagram: 'oseunando',
+    },
+    title,
     version,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-stylus`,
       options: {
         use: [rupture()],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `components`,
+        path: path.join(__dirname, `src`, `components`),
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: path.join(__dirname, `src`, `pages`),
       },
     },
   ],
