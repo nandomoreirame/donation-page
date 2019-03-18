f63e1b6e6a825f1f7fa1e2e514b6520886d0dbc0
Fernando Moreira <nandomoreira.me@gmail.com>
2019-03-18 19:09:05 -0300

    alert variant with styled components

--- src/components/Alert/alert.styled.js
+++ src/components/Alert/alert.styled.js
@@ -1,11 +1,38 @@
 import styled from 'styled-components'
 
+const getAlertVariant = type => {
+  switch (type) {
+  case 'info':
+    return `
+      color: #0c5460
+      background-color: #d1ecf1
+      border-color: #bee5eb
+    `
+  case 'danger':
+  case 'error':
+    return `
+      color: #fff
+      background-color: #ff5252
+      border-color: #ff5252
+    `
+  case 'success':
+  default:
+    return `
+      color: #155724
+      background-color: #d4edda
+      border-color: #c3e6cb
+    `
+  }
+}
+
 export const Alert = styled.div`
   padding: 20px 15px;
   border-radius: 3px;
   display: flex;
   align-items: center;
   border: 1px solid transparent;
+  margin-bottom: 30px;
+  ${ ({ type }) => type && getAlertVariant(type) }
   svg {
     margin: 0;
     padding: 5px;
@@ -22,21 +49,3 @@ export const AlertInner = styled.div`
     margin: 0;
   }
 `
-
-export const alertSuccess = {
-  color: '#155724',
-  backgroundColor: '#d4edda',
-  borderColor: '#c3e6cb',
-}
-
-export const alertInfo = {
-  color: '#0c5460',
-  backgroundColor: '#d1ecf1',
-  borderColor: '#bee5eb',
-}
-
-export const alertError = {
-  color: '#fff',
-  backgroundColor: '#ff5252',
-  borderColor: '#ff5252',
-}
--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -1,33 +1,16 @@
 import React, { Fragment } from 'react'
-import Icon from '../Icons'
+import Icon from 'components/Icons'
 
 import {
   Alert,
-  AlertInner,
-  alertInfo,
-  alertError,
-  alertSuccess
+  AlertInner
 } from './alert.styled'
 
 export default ({ type = 'success', icon, children, ...props }) => {
-  let styles = {}
-  switch (type) {
-  case 'info':
-    styles = { ...alertInfo }
-    break
-  case 'danger':
-  case 'error':
-    styles = { ...alertError }
-    break
-  case 'success':
-  default:
-    styles = { ...alertSuccess }
-    break
-  }
   return (
     <Fragment>
       {children &&
-        <Alert className={`alert alert--${ type }`} style={styles} {...props}>
+        <Alert className={`alert alert--${ type }`} type={type} {...props}>
           {icon && <Icon id={`icon-${ icon }`} fill="currentColor" />}
           <AlertInner className="Alert__inner">{children}</AlertInner>
         </Alert>
