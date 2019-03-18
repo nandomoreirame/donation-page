3c2bae5fe71c60ce884c3708b2657a397e71d65b
Fernando Moreira <nandomoreira.me@gmail.com>
2019-03-18 19:18:17 -0300

    Social icons with styled components

--- src/components/SocialIcons/index.jsx
+++ src/components/SocialIcons/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Icon from '../Icons'
-import styles from './styles'
+import Icon from 'components/Icons'
+import {
+  SocialIconsStyled,
+  TextStyled,
+  LinkStyled
+} from './socialicons.styled'
 
 const defaultProps = {
   size: '32',
@@ -12,25 +16,24 @@ const defaultProps = {
 
 const SocialIcons = ({ textColor, size, fillColor, socialLinks }) => {
   return (
-    <div style={{ ...styles.socialIcons, color: textColor }}>
-      {socialLinks && <span style={styles.socialIconsText}>Compartilhe</span>}
+    <SocialIconsStyled style={{ color: textColor }}>
+      {socialLinks && <TextStyled>Compartilhe</TextStyled>}
 
       {Object.keys(socialLinks).map((item, i) => {
         const link = socialLinks[item]
         if (!link) return null
         return (
-          <a
-            style={{ ...styles.socialIconsLink }}
+          <LinkStyled
             href={link}
             target="_blank"
             rel="noopener noreferrer"
             key={i}
           >
             <Icon id={`icon-${ item }`} fill={fillColor} width={size} height={size} style={{ padding: '4px' }} />
-          </a>
+          </LinkStyled>
         )
       })}
-    </div>
+    </SocialIconsStyled>
   )
 }
 
---
+++ src/components/SocialIcons/socialicons.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const SocialIconsStyled = styled.div`
+  background-color: #fff;
+  display: flex;
+  padding: 5px;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+export const TextStyled = styled.span`
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 800;
+  display: inline-block;
+  margin-right: 10px;
+`
+
+export const LinkStyled = styled.a`
+  display: inline-block;
+  margin-right: 5px;
+  text-decoration: none;
+`
