2d727131b606b3f510c3f26d6553c26d68411a96
Fernando Moreira <nandomoreira.me@gmail.com>
2019-03-18 15:20:21 -0300

    Styled Alert component

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -4,25 +4,25 @@ import {
   AlertInner,
   alertInfo,
   alertError,
-  alertSuccess,
-} from './styled'
+  alertSuccess
+} from './alert.styled'
 
 export default ({ type, children, ...props }) => {
   let styles = {}
   switch (type) {
-    case 'info':
-      styles = { ...alertInfo }
-      break
-    case 'error':
-      styles = { ...alertError }
-      break
-    case 'success':
-    default:
-      styles = { ...alertSuccess }
-      break
+  case 'info':
+    styles = { ...alertInfo }
+    break
+  case 'error':
+    styles = { ...alertError }
+    break
+  case 'success':
+  default:
+    styles = { ...alertSuccess }
+    break
   }
   return (
-    <Alert className={`Alert Alert--${type}`} style={styles} {...props}>
+    <Alert className={`Alert Alert--${ type }`} style={styles} {...props}>
       {children && <AlertInner className="Alert__inner">{children}</AlertInner>}
     </Alert>
   )
