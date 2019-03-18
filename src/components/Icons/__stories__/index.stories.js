e4032a76582b0f3a2b51da8e490bd902abe40532
Fernando Moreira <nandomoreira.me@gmail.com>
2019-03-18 15:30:08 -0300

    Add icons stories

---
+++ src/components/Icons/__stories__/index.stories.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Icon from '../index'
+
+storiesOf('Icons', module)
+  .add('close-outline', () => <Icon id="icon-close-outline" />)
+  .add('close', () => <Icon id="icon-close" />)
+  .add('calendar', () => <Icon id="icon-calendar" />)
+  .add('check', () => <Icon id="icon-check" />)
+  .add('error', () => <Icon id="icon-error" />)
+  .add('facebook', () => <Icon id="icon-facebook" />)
+  .add('information', () => <Icon id="icon-information" />)
+  .add('instagram', () => <Icon id="icon-instagram" />)
+  .add('linkedin', () => <Icon id="icon-linkedin" />)
+  .add('lock', () => <Icon id="icon-lock" />)
+  .add('pinterest', () => <Icon id="icon-pinterest" />)
+  .add('triangle-down', () => <Icon id="icon-triangle-down" />)
+  .add('twitter', () => <Icon id="icon-twitter" />)
+  .add('youtube', () => <Icon id="icon-youtube" />)
